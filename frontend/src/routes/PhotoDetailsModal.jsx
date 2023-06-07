cca2f5c71a8d658612ac0ae4f4484f322be07fe1
anna-flytis <a.mariakhina@gmail.com>
2023-06-07 15:23:23 -0600

    updated for new functions and styles

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,40 +1,14 @@
 import React from 'react';
-import '../styles/PhotoDetailsModal.scss';
-import ModalPhotoList from '../components/ModalPhotoList';
+import PhotoList from '../components/PhotoList';
 import PhotoFavButton from '../components/PhotoFavButton';
+import '../styles/PhotoDetailsModal.scss'
+import '../styles/PhotoFavButton.scss';
 
 export const PhotoDetailsModal = (props) => {
-
-  const { closeModal, like, PhotoData, clickedImgID } = props;
-  console.log(PhotoData);
-  console.log("hello", clickedImgID);
-  console.log(PhotoData[0].id);
-
-  const displayOneImg = () => {
-
-    const clickedImageInfo = PhotoData.filter(photo => photo.id === clickedImgID);
-    return (
-      <div className="card">
-        <PhotoFavButton like={like} photo_id={clickedImageInfo[0].id} />
-        <img src={clickedImageInfo[0].urls.full} />
-        <div className='photo-list--user-details'>
-        <img className="photo-list--user-profile" src={clickedImageInfo[0].user.profile} />
-          <div className='photo-list--user-info'>
-            <span>{clickedImageInfo[0].user.name}</span>
-            <div className="photo-list--user-location">
-              {clickedImageInfo[0].location.city}, {clickedImageInfo[0].location.country}
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-
-  };
-
+  const { photos, selectPhoto, favouritedPhotos, setFavouritedPhotos } = props;
   return (
     <div className='photo-details-modal'>
-      <button className='photo-details-modal--close-button'
-        onClick={closeModal}>
+      <button className='photo-details-modal--close-button' onClick={props.closeModal}>
         <svg width="24" height="24" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
           <g clipPath="url(#clip0_428_287)">
             <path d="M14.0625 3.9375L3.9375 14.0625" stroke="black" strokeLinecap="round" strokeLinejoin="round" />
@@ -48,13 +22,31 @@ export const PhotoDetailsModal = (props) => {
         </svg>
       </button>
 
-      {displayOneImg()}
+      <div className='photo-details-modal--image'>
+        <PhotoFavButton
+          favouritedPhotos={favouritedPhotos}
+          setFavouritedPhotos={setFavouritedPhotos}
+          id={selectPhoto.id} />
+        <img src={selectPhoto.imageSource} alt="" />
+      </div>
 
-      <span id = "smp"><b>Similar Photos:</b></span>
+      <div className='photo-details-modal--photographer-details'>
+        <img src={selectPhoto.imageSource} alt="" className='photo-details-modal--photographer-profile' />
+        <div className='photo-details-modal--photographer-info'>
+          <p>{selectPhoto.username}</p>
+          <p className='photo-details-modal--photographer-location'>{selectPhoto.city}, {selectPhoto.country}</p>
+        </div>
+      </div>
 
-      <ModalPhotoList photos={PhotoData} like={like} />
+      <div className="divider"></div>
+
+      <section className='photo-details-modal--images'>
+        <h1 className='photo-details-modal--header'>Similar photos:</h1>
+        <PhotoList photos={photos} favouritedPhotos={favouritedPhotos} setFavouritedPhotos={setFavouritedPhotos} />
+      </section>
+
     </div>
-  );
-};
+  )
+}
 
 export default PhotoDetailsModal;
