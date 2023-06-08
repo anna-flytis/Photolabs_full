2e5dea01bbb5f39e8c2d8f3f0d4aa82be85887cd
anna-flytis <a.mariakhina@gmail.com>
2023-06-07 20:39:03 -0600

    moved useState and useEffect from app component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,75 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React from 'react';
 import './App.scss';
 import useApplicationData from './hooks/useApplicationData';
 import HomeRoute from './routes/HomeRoute';
-import PhotoDetailsModal from './routes/PhotoDetailsModal'
-
+import PhotoDetailsModal from './routes/PhotoDetailsModal';
 
 const App = () => {
   const {
+    photos,
+    topics,
     modal,
     openModal,
     closeModal,
     selectPhoto,
     favouritedPhotos,
-    setFavouritedPhotos } = useApplicationData();
-
-  const [photos, setPhotos] = useState([]);
-  const [topics, setTopics] = useState([])
-
-  useEffect(() => {
-    axios({
-      url: "http://localhost:8001/api/photos",
-      method: "GET",
-      dataResponse: "json"
-    })
-      .then((res) => {
-        setPhotos(prev => res.data)
-      })
-      .catch((error) => {
-        console.error('Error fetching photos:', error);
-      });
-  }, []);
-
-  useEffect(() => {
-    axios({
-      url: "http://localhost:8001/api/topics",
-      method: "GET",
-      dataResponse: "json"
-    })
-      .then((res) => {
-        setTopics(prev => res.data)
-      })
-      .catch((error) => {
-        console.error('Error fetching photos by topic:', error);
-      });
-  }, []);
-
-  const handleTopicClick = (topicId) => {
-    axios
-      .get(`http://localhost:8001/api/topics/photos/${topicId}`)
-      .then((res) => {
-        setPhotos(prev => res.data);
-      })
-      .catch((error) => {
-        console.error('Error fetching photos by topic:', error);
-      });
-  };
-
-  const handleHomepageClick = () => {
-    axios({
-      url: "http://localhost:8001/api/photos",
-      method: "GET",
-      dataResponse: "json"
-    })
-      .then((res) => {
-        setPhotos(prev => res.data)
-      })
-      .catch((error) => {
-        console.error('Error fetching photos', error);
-      });
-  };
+    setFavouritedPhotos,
+    handleTopicClick,
+    handleHomepageClick,
+  } = useApplicationData();
 
   return (
     <div>
@@ -77,19 +24,23 @@ const App = () => {
         photos={photos}
         topics={topics}
         openModal={openModal}
-        favouritedPhotos={favouritedPhotos} setFavouritedPhotos={setFavouritedPhotos}
+        favouritedPhotos={favouritedPhotos}
+        setFavouritedPhotos={setFavouritedPhotos}
         handleTopicClick={handleTopicClick}
-        handleHomepageClick={handleHomepageClick} />
+        handleHomepageClick={handleHomepageClick}
+      />
 
-      {modal &&
+      {modal && (
         <PhotoDetailsModal
           closeModal={closeModal}
           selectPhoto={selectPhoto}
           photos={photos}
-          favouritedPhotos={favouritedPhotos} setFavouritedPhotos={setFavouritedPhotos}
-        />}
+          favouritedPhotos={favouritedPhotos}
+          setFavouritedPhotos={setFavouritedPhotos}
+        />
+      )}
     </div>
   );
 };
 
-export default App
+export default App;
--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,55 +1,116 @@
-import { useReducer } from 'react';
+import { useReducer, useEffect } from 'react';
+import axios from 'axios';
+
+const initialState = {
+  photos: [],
+  topics: [],
+  favouritedPhotos: [],
+  selectPhoto: null,
+  modal: false,
+};
 
 const reducer = (state, action) => {
-switch (action.type) {
-case 'SET_FAVOURITED_PHOTOS':
-return {
-...state,
-favouritedPhotos: action.payload
-};
-case 'SET_SELECT_PHOTO':
-return {
-...state,
-selectPhoto: action.payload
-};
-case 'SET_MODAL':
-return {
-...state,
-modal: action.payload
-};
-default:
-return state;
-}
+  switch (action.type) {
+    case 'SET_PHOTOS':
+      return {
+        ...state,
+        photos: action.payload,
+      };
+    case 'SET_TOPICS':
+      return {
+        ...state,
+        topics: action.payload,
+      };
+    case 'SET_FAVOURITED_PHOTOS':
+      return {
+        ...state,
+        favouritedPhotos: action.payload,
+      };
+    case 'SET_SELECT_PHOTO':
+      return {
+        ...state,
+        selectPhoto: action.payload,
+      };
+    case 'SET_MODAL':
+      return {
+        ...state,
+        modal: action.payload,
+      };
+    default:
+      return state;
+  }
 };
 
 const useApplicationData = () => {
-  const initialState = {
-    favouritedPhotos: [],
-    selectPhoto: {},
-    modal: false
-    };
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(() => {
+    axios
+      .get('http://localhost:8001/api/photos')
+      .then((res) => {
+        dispatch({ type: 'SET_PHOTOS', payload: res.data });
+      })
+      .catch((error) => {
+        console.error('Error fetching photos:', error);
+      });
+  }, []);
 
-const openModal = (photo) => {
-dispatch({ type: 'SET_SELECT_PHOTO', payload: photo });
-dispatch({ type: 'SET_MODAL', payload: true });
-};
+  useEffect(() => {
+    axios
+      .get('http://localhost:8001/api/topics')
+      .then((res) => {
+        dispatch({ type: 'SET_TOPICS', payload: res.data });
+      })
+      .catch((error) => {
+        console.error('Error fetching topics:', error);
+      });
+  }, []);
 
-const closeModal = () => {
-dispatch({ type: 'SET_SELECT_PHOTO', payload: null });
-dispatch({ type: 'SET_MODAL', payload: false });
-};
+  const openModal = (photo) => {
+    dispatch({ type: 'SET_SELECT_PHOTO', payload: photo });
+    dispatch({ type: 'SET_MODAL', payload: true });
+  };
 
-return {
-modal: state.modal,
-openModal,
-closeModal,
-selectPhoto: state.selectPhoto,
-favouritedPhotos: state.favouritedPhotos,
-setFavouritedPhotos: (favouritedPhotos) =>
-dispatch({ type: 'SET_FAVOURITED_PHOTOS', payload: favouritedPhotos })
-};
+  const closeModal = () => {
+    dispatch({ type: 'SET_SELECT_PHOTO', payload: null });
+    dispatch({ type: 'SET_MODAL', payload: false });
+  };
+
+  const handleTopicClick = (topicId) => {
+    axios
+      .get(`http://localhost:8001/api/topics/photos/${topicId}`)
+      .then((res) => {
+        dispatch({ type: 'SET_PHOTOS', payload: res.data });
+      })
+      .catch((error) => {
+        console.error('Error fetching photos by topic:', error);
+      });
+  };
+
+  const handleHomepageClick = () => {
+    axios
+      .get('http://localhost:8001/api/photos')
+      .then((res) => {
+        dispatch({ type: 'SET_PHOTOS', payload: res.data });
+      })
+      .catch((error) => {
+        console.error('Error fetching photos:', error);
+      });
+  };
+
+  return {
+    photos: state.photos,
+    topics: state.topics,
+    modal: state.modal,
+    openModal,
+    closeModal,
+    selectPhoto: state.selectPhoto,
+    favouritedPhotos: state.favouritedPhotos,
+    setFavouritedPhotos: (favouritedPhotos) =>
+      dispatch({ type: 'SET_FAVOURITED_PHOTOS', payload: favouritedPhotos }),
+    handleTopicClick,
+    handleHomepageClick,
+  };
 };
 
 export default useApplicationData;
